c16fb5fcedcedbabc59f6600cd4c0842ef9691d1
mousse-ldlc <sebastien.skorupski@gmail.com>
2019-02-26 22:16:57 +0100

    [feature/bootstrap-vue] Add bootsrap-vue

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,7 @@ sync(store, router);
 
 // Plugins
 import vClickOutside from 'v-click-outside';
+import BootstrapVue from 'bootstrap-vue'
 
 // Components
 import { AppComponent } from './components/layout/app';
@@ -20,12 +21,14 @@ import { AppComponent } from './components/layout/app';
 // Styles
 import 'bootstrap/dist/css/bootstrap.css';
 import './assets/styles/main.scss';
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 // Setup
 Vue.config.productionTip = false;
 
 // Plugins
 Vue.use(vClickOutside);
+Vue.use(BootstrapVue);
 
 // Vue init
 new Vue({
